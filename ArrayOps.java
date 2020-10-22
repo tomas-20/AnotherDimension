dd1af0de78577c464e25691854dd5b46b52b13bb
tomas-20 <tacuna20@stuy.edu>
2020-10-22 13:29:10 -0400

    did the sum

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,3 +1,9 @@
 public class ArrayOps {
-  
+  public static int sum(int[] arr) {
+    int output = 0;
+    for (int i = 0; i < arr.length; i ++) {
+      output += arr[i];
+    }
+    return output;
+  }
 }
--- Tester.java
+++ Tester.java
@@ -1,4 +1,6 @@
 public class Tester {
   public static void main(String[] args) {
+    int[] cheese = {8, 42, 2187, 379};
+    System.out.println(ArrayOps.sum(cheese));
   }
 }
