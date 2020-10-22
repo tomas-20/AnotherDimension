324d76eb5a521272537756e5e30ba5ba262330a1
tomas-20 <tacuna20@stuy.edu>
2020-10-22 19:11:27 -0400

    made a super fancy state of the art testing system

--- Tester.java
+++ Tester.java
@@ -1,12 +1,29 @@
 import java.util.Arrays;
 public class Tester {
+  public static int[][] splitArray(int[] arr, int rowSize) {
+    int rows = arr.length / rowSize;
+    int[][] output = new int[rows][rowSize];
+    for (int i = 0; i < rows; i ++) {
+      for (int j = 0; j < rowSize; j ++) {
+        output[i][j] = arr[i * rowSize + j];
+      }
+    }
+    return output;
+  }
   public static void main(String[] args) {
-    int[] cheese = {8, 42, 2187, 379};
-    System.out.println(ArrayOps.sum(cheese));
-    System.out.println(ArrayOps.largest(cheese));
-    int[][] queso = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    System.out.println(Arrays.toString(ArrayOps.sumRows(queso)));
-    System.out.println(Arrays.toString(ArrayOps.largestInRows(queso)));
-    System.out.println(ArrayOps.sum(queso));
+    int rowSize = Integer.parseInt(args[0]);
+    int length = args.length - 1;
+    int[] arr = new int[length];
+    for (int i = 0; i < length; i ++) {
+      arr[i] = Integer.parseInt(args[i + 1]);
+    }
+    int[][] arr2d = splitArray(arr, rowSize);
+    System.out.println(Arrays.toString(arr));
+    System.out.println(Arrays.deepToString(arr2d));
+    System.out.println(ArrayOps.sum(arr));
+    System.out.println(ArrayOps.largest(arr));
+    System.out.println(Arrays.toString(ArrayOps.sumRows(arr2d)));
+    System.out.println(Arrays.toString(ArrayOps.largestInRows(arr2d)));
+    System.out.println(ArrayOps.sum(arr2d));
   }
 }
