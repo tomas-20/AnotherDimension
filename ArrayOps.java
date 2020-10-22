2eca99fd702d1dae0e5ae6d579a8e8af4fdceb61
tomas-20 <tacuna20@stuy.edu>
2020-10-22 13:45:56 -0400

    did the largest

--- ArrayOps.java
+++ ArrayOps.java
@@ -6,4 +6,14 @@ public class ArrayOps {
     }
     return output;
   }
+  public static int largest(int[] arr) {
+    int output = arr[0];
+    for (int i = 1; i < arr.length; i ++) {
+      int n = arr[i];
+      if (n > output) {
+        output = n;
+      }
+    }
+    return output;
+  }
 }
--- Tester.java
+++ Tester.java
@@ -2,5 +2,6 @@ public class Tester {
   public static void main(String[] args) {
     int[] cheese = {8, 42, 2187, 379};
     System.out.println(ArrayOps.sum(cheese));
+    System.out.println(ArrayOps.largest(cheese));
   }
 }
